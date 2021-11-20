2f292177d60b4994cc51fbe1e797e695c0de6b97
Adebayor <idrisadeebayorr12@gmail.com>
2021-11-20 23:16:00 +0100

    first commit

---
+++ src/Components/Profile/Address.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Address = () => {
+  return (
+    <div>
+      <h2>No 35 Olaleye, Gbagada Lagos</h2>
+      <h3>I am an intern software developer at GOMYCODE, i love to code and try new things everyday.</h3>
+      
+    </div>
+  );
+};
+
+export default Address;
---
+++ src/Components/Profile/FullName.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const FullName = () => {
+  return (
+    <div>
+      <div>
+        <img src="./image.jpg" alt="Profilephoto" />
+      </div>
+      <h1 className="name">Adebayo Idris</h1>
+      {/* <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure in ducimus,
+          tenetur facere repellendus ipsa beatae minus est laudantium nobis tempora
+          suscipit sunt tempore reprehenderit nulla necessitatibus! Eius sed
+          provident iusto dicta corrupti itaque, ducimus quo illo cupiditate,
+          quaerat blanditiis, quod accusamus doloremque <br />
+          Neque unde nostrum dignissimos ad quam ratione.</p> */}
+    </div>
+  );
+};
+
+export default FullName;
